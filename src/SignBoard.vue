9992242e7e84212e5e96f2ba3dc8c2c54187d629
Yxw <2455626085@qq.com>
2023-05-27 12:38:09 +0800

    基础组件引入

--- src/SignBoard.vue
+++ src/SignBoard.vue
@@ -1,6 +1,33 @@
 <template>
-	<div>6666666666666</div>
+	<button @click="open = true">Open Modal</button>
+	<div class="p-3 bg-green-500 text-white">Hello Tailwind CSS</div>
+	<Teleport to="body">
+		<div v-if="open" class="modal">
+			<VueDragResizeRotate :x="0" :y="0" :w="200" :h="200">
+				<h1 class="text-3xl font-bold underline">Hello world!</h1>
+			</VueDragResizeRotate>
+			<button @click="open = false">Close</button>
+		</div>
+	</Teleport>
 </template>
-<script setup></script>
+<script setup>
+	import VueDragResizeRotate from '@gausszhou/vue3-drag-resize-rotate'
+	import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+	const open = ref(true)
+	const modelClose = () => {
+		console.log(12121)
+	}
+</script>
+
+<style scoped>
+	.modal {
+		position: absolute;
+		background: #fff;
+		top: 0;
+		left: 0;
+		z-index: 999;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp, h } from 'vue'
 import SignBoard from './SignBoard.vue'
+import "@gausszhou/vue3-drag-resize-rotate/lib/bundle.esm.css";
+import "./assets/tailwind.css"
 try {
     plugin.onConfig(function () {
         const root = document.createElement('div')
@@ -7,10 +9,11 @@ try {
         return root;
     });
 } catch (error) {
-    const Element = document.getElementsByTagName('body')
-    const tip = document.createElement('h2')
+    const Element = document.getElementsByClassName("test")
+    const tip = document.createElement('div')
     tip.innerHTML = '插件暂不支持外部运行!!!'
     Element[0].appendChild(tip)
-    console.error('插件暂不支持外部运行!!!')
+    // console.error('插件暂不支持外部运行!!!')
+    createApp(SignBoard).mount(tip);
 }
 
