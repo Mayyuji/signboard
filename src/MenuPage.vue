8c27371d1aad1a9ca1b265cf07dec89eeddca242
Yxw <2455626085@qq.com>
2023-08-02 16:12:25 +0800

    11

--- src/MenuPage.vue
+++ src/MenuPage.vue
@@ -54,10 +54,10 @@
   <Waifu :control="showWaifu && !panelShow" ref="waifuRef"></Waifu>
 </template>
 <script setup>
-  import { getImageSize } from './utils/utrl'
   import Waifu from './pages/waifunew.vue'
-  import Panel from './pages/Panel'
+  import Panel from './pages/panelnew.vue'
   import Switch from './components/switch.vue'
+  import { getImageSize } from './utils/utrl'
   import _s from './utils/storge'
 
   import { ref, reactive, toRaw, unref, onMounted } from 'vue'
